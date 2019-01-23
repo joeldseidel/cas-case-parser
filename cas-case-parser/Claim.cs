d4269291a1ee5031f978daec94154e7446a1ed82
Joel Seidel <joeldseidel@gmail.com>
2019-01-22 21:12:38 -0500

    read file from csv

---
+++ cas-case-parser/Claim.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cas_case_parser
+{
+    public class Claim
+    {
+        private int typeId, yearsOld, userAge, riskId;
+        private decimal claimValue;
+        private bool hasCase;
+
+        public Claim(string[] claimComponents)
+        {
+            this.typeId = Convert.ToInt32(claimComponents[0]);
+            this.hasCase = claimComponents[1].Equals("Yes") ? true : false;
+            this.yearsOld = DateTime.Now.Year - Convert.ToInt32(claimComponents[2]);
+            this.userAge = Convert.ToInt32(claimComponents[3]);
+            this.riskId = claimComponents[4].Equals("High") ? 3 : claimComponents[4].Equals("Medium") ? 2 : 1;
+            this.claimValue = determineClaimValue(claimComponents[5]);
+        }
+
+        /// <summary>
+        /// Convert claim type name to corresponding risk value
+        /// </summary>
+        /// <param name="claimName">name of the claim from claim components</param>
+        /// <returns>value of the claim</returns>
+        private decimal determineClaimValue(string claimName)
+        {
+            switch (claimName)
+            {
+                case "Screen":
+                    return 200;
+                case "Battery":
+                    return 150;
+                default:
+                    return 99;
+            }
+        }
+    }
+}
---
+++ cas-case-parser/Program.cs
@@ -0,0 +1,33 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.IO;
+using System.Collections;
+using System.Threading.Tasks;
+
+namespace cas_case_parser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Press any key to being parsing file");
+            Console.ReadKey();
+            Queue claimsQueue = readFile();
+        }
+
+        static Queue readFile()
+        {
+            Queue claimsQueue = new Queue();
+            const string fileLocation = @"C:\Users\Joel\Desktop\claims.csv";
+            StreamReader fileReader = new StreamReader(fileLocation);
+            //Throw out the first line containing the headers of the table
+            fileReader.ReadLine();
+            do
+            {
+                string[] recordContents = fileReader.ReadLine().Split(',');
+                claimsQueue.Enqueue(new Claim(recordContents));
+            } while (fileReader.Peek() != -1);
+            return claimsQueue;
+        }
+    }
+}
